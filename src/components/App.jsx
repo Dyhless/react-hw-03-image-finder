0a477dbb7569845462523607ac3bc16d89849d87
Dmytro Medvediev <dsisitkin@gmail.com>
2023-07-16 22:40:11 +0200

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import * as SearchBar from './Searchbar/Searchbar';
+import {
+  Button,
+  ImageGallery,
+  ImageGalleryItem,
+  Loader,
+  Modal,
+  Searchbar,
+} from 'components';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Searchbar class="searchbar">
+          <form class="form">
+            <button type="submit" class="button">
+              <span class="button-label">Search</span>
+            </button>
+
+            <input
+              class="input"
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+            />
+          </form>
+        </Searchbar>
+
+        <ImageGallery class="gallery">
+          <ImageGalleryItem class="gallery-item">
+            <img src="" alt="" />
+          </ImageGalleryItem>
+        </ImageGallery>
+
+        <Button>Load more</Button>
+
+        <div class="overlay">
+          <Modal class="modal">
+            <img src="" alt="" />
+          </Modal>
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_KEY = '37053026-4150c05ae3a340932daa6308e';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.headers.common['Authorization'] = API_KEY;
+axios.defaults.params = {
+  orientation: 'landscape',
+  per_page: 12,
+};
+
+export const getImages = async (query, page) => {
+  try {
+    const response = await axios.get(`?q=${query}&page=${page}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+};
+
+// export class Searchbar extends Component {}
